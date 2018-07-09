d3c8bae51cad00b201b368f247fd0d4f99c2ea86
Shoaib Shakeel <shoaibshakeel381@yahoo.com>
2018-07-09 18:18:41 +0500

    Update Input.bat

--- VNRSharedDictFilter/Input.bat
+++ VNRSharedDictFilter/Input.bat
@@ -1,7 +1,6 @@
-@REM  ./DictFilter.exe
-@REM  ./DictFilter.exe gamespecific 46072,55333,70716,60285,71614,67544,48109,16087,69510
-@REM  ./DictFilter.exe element      special true
-@REM  ./DictFilter.exe remove       dictName.xml 55333,46072
+@REM  ./DictFilter.exe              Dictionary.xml
+@REM  ./DictFilter.exe gamespecific Dictionary.xml   46072,55333,70716,60285,71614,67544,48109,16087,69510
+@REM  ./DictFilter.exe element      Dictionary.xml   special true
+@REM  ./DictFilter.exe remove       Dictionary.xml   dictName.xml 55333,46072
 @REM  ./DictFilter.exe merge        fileA.xml fileB.xml
-@REM  ./DictFilter.exe print
-@DictFilter.exe gamespecific 46072,55333,70716,60285,71614,67544,48109,16087,69510
+@DictFilter.exe gamespecific Dictionary.xml 46072,55333,70716,60285,71614,67544,48109,16087,69510
